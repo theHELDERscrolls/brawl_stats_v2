01c926d3f616207ef347b6cb4ec817b9f2ba58ff
Helder <manuhelderruiz@gmail.com>
2025-10-24 17:40:02 +0200

    feat: update trust proxy setting for improved request handling

--- api-proxy-server/src/index.ts
+++ api-proxy-server/src/index.ts
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.set("trust proxy", true);
+app.set("trust proxy", 1);
 
 const allowedOrigins = process.env.ALLOWED_ORIGINS
   ? process.env.ALLOWED_ORIGINS.split(",")
